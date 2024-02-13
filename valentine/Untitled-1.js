492066f13c087ad36a309df1355c6fd07cb2cb9e
MarselaBiak <159937068+MarselaBiak@users.noreply.github.com>
2024-02-14 00:02:23 +0300

    Add files via upload

---
+++ valentine/Untitled-1.js
@@ -0,0 +1,33 @@
+$('.envelope').on('click', function(e) {
+    $('.envelopeTop').toggleClass('envelopeTopOpen');
+}) 
+$('.envelope').on('click', function(e) {
+    $('.letter').toggleClass('letterOpen');
+}) 
+$('.envelope').on('click', function(e) {
+    $('.photo').toggleClass('photoOpen');
+}) 
+$('.envelope').on('click', function(e) {
+    $('.heart1').toggleClass('heart1_animation');
+}) 
+$('.envelope').on('click', function(e) {
+    $('.heart2').toggleClass('heart1_animation');
+}) 
+$('.envelope').on('click', function(e) {
+    $('.heart3').toggleClass('heart3_animation');
+}) 
+$('.envelope').on('click', function(e) {
+    $('.heart4').toggleClass('heart4_animation');
+}) 
+$('.envelope').on('click', function(e) {
+    $('.heart5').toggleClass('heart3_animation');
+}) 
+$('.envelope').on('click', function(e) {
+    $('.heart6').toggleClass('heart1_animation');
+}) 
+$('.envelope').on('click', function(e) {
+    $('.heart7').toggleClass('heart1_animation');
+}) 
+$('.envelope').on('click', function(e) {
+    $('.heart8').toggleClass('heart8_animation');
+}) 
